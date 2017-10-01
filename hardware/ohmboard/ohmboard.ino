bc59217a1927e16f79ebb0975f2ae96fa12be67e
vaskoiii <j3vasko@gmail.com>
2017-10-01 22:20:45 +0000

    look at }])> <([{ as arrows

--- hardware/ohmboard/ohmboard.ino
+++ hardware/ohmboard/ohmboard.ino
@@ -213,7 +213,7 @@ const uint16_t keymap[4][2][16] = {
 
 	// .+*^= %XX-,
 	// 13579 86420
-	// {[(<   >)]}
+	// }])>   <([{
 	{ // 01
 		{ // 0
 			KEY_9, // 0000
@@ -221,10 +221,10 @@ const uint16_t keymap[4][2][16] = {
 			KEY_3, // 0010
 			KEY_5, // 0011
 			KEY_7, // 0100
-			KEY_COMMA, // shifted to < // 0110
-			KEY_9, // shifted to ( // 0101
-			KEY_LEFT_BRACE, // 0111
-			KEY_LEFT_BRACE, // shifted to { // 1000
+			KEY_PERIOD, // shifted to > // 0110
+			KEY_0, // shifted to ) // 0101
+			KEY_RIGHT_BRACE, // 0111
+			KEY_RIGHT_BRACE, // shifted to } // 1000
 			KEY_PAGE_DOWN, // 1001
 			KEY_TAB, //  1010
 			KEY_PERIOD, // 1011
@@ -239,10 +239,10 @@ const uint16_t keymap[4][2][16] = {
 			KEY_2, // 0010
 			KEY_4, // 0011
 			KEY_6, // 0100
-			KEY_PERIOD, // shifted to > // 0110
-			KEY_0, // shifted to ) // 0101
-			KEY_RIGHT_BRACE, // 0111
-			KEY_RIGHT_BRACE, // shifted to } // 1000
+			KEY_COMMA, // shifted to < // 0110
+			KEY_9, // shifted to ( // 0101
+			KEY_LEFT_BRACE, // 0111
+			KEY_LEFT_BRACE, // shifted to { // 1000
 			KEY_PAGE_UP, // 1001
 			KEY_TAB, // shifted to "shift tab" // TODO reverse tab possible? // 1010
 			KEY_COMMA, // 1011
