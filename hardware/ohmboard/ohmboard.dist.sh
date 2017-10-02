8c3423ce444eec0a35538e3dcb9ed9a36bf32f1f
vaskoiii <j3vasko@gmail.com>
2017-10-02 00:05:06 +0000

    use rated speed for teensy 3.1 and 3.2

--- hardware/ohmboard/ohmboard.dist.sh
+++ hardware/ohmboard/ohmboard.dist.sh
@@ -9,7 +9,7 @@ teensyduino_path=/path/to/teensyduino
 $teensyduino_path/arduino-builder -build-path $ohmboard_path/build -hardware $teensyduino_path/hardware -tools $teensyduino_path/hardware/tools/avr -tools $teensyduino_path/tools-builder -libraries $teensyduino_path/libraries -fqbn teensy:avr:teensy31 $ohmboard_path/ohmboard.ino
 
 # requires adding the following lines at the top of the teensy31 section in $teensyduino_path/hardware/teensy/avr/boards.txt
-# teensy31.build.fcpu=48000000
+# teensy31.build.fcpu=72000000
 # teensy31.build.flags.optimize=-O2
 # teensy31.build.flags.ldspecs=
 # teensy31.build.keylayout=US_ENGLISH
